5bd20d6c1f90b2b86f51ddbbf15bc110285db276
tomitoyo <2073941-tomitoyo@users.noreply.replit.com>
2023-07-13 21:17:19 +0000

    book lib

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
 const newBookBtn = document.getElementById('btn');
 const addBookBtn = document.getElementById('addBook');
-const popForm = document.getElementById('form');
-const mainPage = document.querySelector('main')
+const form = document.getElementById('form');
+const mainPage = document.querySelector('#tbody')
+
 newBookBtn.addEventListener('click', () => {
-  if(popForm.style.display === 'none'){
-    popForm.style.display = 'block';
-  } else {
-    popForm.style.display = 'none';
-  }
+  form.style.display = (form.style.display === 'none' ? 'block' : 'none')
 })
 
-addBookBtn.addEventListener('click', addBook);
+addBookBtn.addEventListener('click', addBookToLibrary);
 
+let newLib = []
 
 // book constructor
 function Book(author, title, pages, read){
-  this.author = form.author.value;
-  this.title = form.title.value;
-  this.pages = form.pages.value;
-  this.read = form.read.value;
+  this.author = author.value;
+  this.title = title.value;
+  this.pages = pages.value;
+  this.read = read.value;
 };
-  let newLib = []
-  let newBk;
-  console.log(newLib)
-function addBook(event){
-  event.preventDefault();
-  popForm.style.display = 'none';
 
-  newBk = new Book(author, title, pages, read);
+const newBk = new Book(author, title, pages, read);
+
+function addBookToLibrary(event){
+  event.preventDefault();
+  form.style.display = 'none';
   newLib.push(newBk)
   form.reset();
+  setData();
   render();
 }
 
-function render(){
-  let title = document.createElement('p')
-  const author = document.createElement('p')
-  const pages = document.createElement('p')
-  const read = document.createElement('p')
+function removeBook(title) {
+    newLib = newLib.filter((itm) => itm.title !== title);
+    render();
+  }
+
+function render() {
+  mainPage.innerHTML = '';
 
   newLib.forEach((itm) => {
-    title = itm.title;
-  })
-  mainPage.appendChild(title)
+    const page = `
+      <tr>
+        <td>${itm.author}</td>
+        <td>${itm.title}</td>
+        <td>${itm.pages}</td>
+        <td>${itm.read}</td>
+        <td><button onclick="removeBook('${itm.title}')" id="removeBtn">remove</button></td>
+      </tr>
+    `;
+    mainPage.insertAdjacentHTML('afterbegin', page);
+      setData();
+  });
 }
+
+function setData() {
+    localStorage.setItem(`newLib`, JSON.stringify(newLib));
+}
+
+function restore() {
+  const storedLib = localStorage.getItem('newLib');
+  newLib = storedLib ? JSON.parse(storedLib) : [];
+  render();
+}
+
+restore();
+
